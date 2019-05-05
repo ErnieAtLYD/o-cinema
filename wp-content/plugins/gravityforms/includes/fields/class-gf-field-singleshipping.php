cee1dc974fbee282ad5ff7803c377d8b6b71c485
Ernie Hsiung <ernie@whereby.us>
2019-05-05 13:01:57 -0400

    build: reset, get true archive

--- wp-content/plugins/gravityforms/includes/fields/class-gf-field-singleshipping.php
+++ wp-content/plugins/gravityforms/includes/fields/class-gf-field-singleshipping.php
@@ -36,7 +36,7 @@ class GF_Field_SingleShipping extends GF_Field {
 
 		$price = esc_attr( $price );
 
-		return "<div class='ginput_container'>
+		return "<div class='ginput_container ginput_container_singleshipping'>
 					<input type='hidden' name='input_{$id}' value='{$price}' class='gform_hidden'/>
 					<span class='ginput_shipping_price' id='{$field_id}'>" . GFCommon::to_money( $price, $currency ) . '</span>
 				</div>';
@@ -50,6 +50,14 @@ class GF_Field_SingleShipping extends GF_Field {
 		parent::sanitize_settings();
 		$price_number    = GFCommon::to_number( $this->basePrice );
 		$this->basePrice = GFCommon::to_money( $price_number );
+	}
+
+	public function get_value_default() {
+		$value = $this->is_form_editor() ? $this->defaultValue : GFCommon::replace_variables_prepopulate( $this->defaultValue );
+		if( rgblank( $value ) ) {
+			$value = $this->basePrice;
+		}
+		return $value;
 	}
 }
 
