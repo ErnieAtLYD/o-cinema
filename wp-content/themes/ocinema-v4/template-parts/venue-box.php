ededefe41fde152cacc88229c1fd0157a0212805
Ernie Hsiung <ernie@whereby.us>
2020-03-25 17:26:04 -0400

    don't show title if no address

--- wp-content/themes/ocinema-v4/template-parts/venue-box.php
+++ wp-content/themes/ocinema-v4/template-parts/venue-box.php
@@ -6,6 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <a class="venue-thumb" href="<?php echo esc_url( tribe_get_venue_link( $venue, false ) ); ?>" style="display:block;position:relative; background:url(<?php the_field( 'venue_banner', $venue ); ?>) center center; background-size:cover; height:14em; margin-bottom:1em;">
 	<div style="background: rgba(0, 0, 0, 0.67); position:absolute; bottom:0; width:100%; color:#fff;">
+		<?php if ( ! empty( tribe_get_address( $venue ) ) ) : ?>
 		<div style="margin:10px;">
 			<h3 style="margin:0; line-height:30px;">
 				<?php return_fancy_html_for_venue( $venue ); ?>
@@ -16,5 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 				<?php echo esc_html( tribe_get_phone( $venue ) ); ?>
 			</span>
 		</div>
+		<?php endif; ?>
 	</div>
 </a>
